3496c901a055e659d6341889cb96c1d1d256a9d2
mohamedashrafalii <mohamed.alibayoumy@student.guc.edu.eg>
2020-03-11 00:02:53 +0200

    deploy

--- api/routes/auth.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth.controller");
-const {
-  Register,get_Allusers,Login}=authController
-  router.post("/Register", Register);
-  router.get("/",get_Allusers);
-  router.post("/Login",Login)
-  module.exports = router;
