2e1a36be590352528a25e68f05056ad534e79f52
mohamedashrafalii <47298322+mohamedashrafalii@users.noreply.github.com>
2020-03-21 18:08:50 +0200

    Update navbar.jsx

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -90,7 +90,7 @@ export default function NavTabs(props) {
           }
           <button style={{background:"inherit",color:"white"}} onClick={async()=>{localStorage.removeItem('token')
           localStorage.removeItem('type')
-          await axios.get("https://pharma-system.herokuapp.com/Logout")
+          await axios.get("https://pharma-system.herokuapp.com/api/auth/Logout")
                           window.location.href='https://pharmacystem.herokuapp.com/'
 
                         }}>LOGOUT</button>
